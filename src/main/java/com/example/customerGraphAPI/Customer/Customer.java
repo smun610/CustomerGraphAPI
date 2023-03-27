db97720ca6fb7581fe18aaab1ea373e222d3b577
smun610 <samad-munthakim@hotmail.co.uk>
2023-03-27 12:46:18 +0100

    Updating lombok annotation to use @Data, to reduce verbosity

--- src/main/java/com/example/customerGraphAPI/Customer/Customer.java
+++ src/main/java/com/example/customerGraphAPI/Customer/Customer.java
@@ -4,16 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 public class Customer {
     @Id
